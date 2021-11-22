9c058ee584893f8a0d12652d9f7ff0c9b967e8b0
LohasOT <oliverhoang93@gmail.com>
2021-11-22 15:41:29 -0800

    working today weather + next 5 days

--- script.js
+++ script.js
@@ -16,63 +16,62 @@ document.getElementById('search').addEventListener('click', event => {
       const latitude = weather.city.coord.lat
       const longtitude = weather.city.coord.lon
 
-      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longtitude}&units=imperial&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
         .then(resp => {
-
+          let today = resp.data
           const uvi = resp.data.current.uvi
           const city = weather.city.name
 
           let current = {
-            date: weather.list[0].dt_txt,
-            temp: weather.list[0].main.temp,
-            humid: weather.list[0].main.humidity,
-            wind: weather.list[0].wind.speed,
-            icon: weather.list[0].weather[0].icon,
+            temp: today.current.temp,
+            humid: today.current.humidity,
+            wind: today.current.wind_speed,
+            icon: today.current.weather[0].icon,
             uvi: uvi
           }
 
           let day1 = {
+            date: weather.list[0].dt_txt,
+            temp: weather.list[0].main.temp,
+            humid: weather.list[0].main.humidity,
+            wind: weather.list[0].wind.speed,
+            icon: weather.list[0].weather[0].icon
+          }
+
+          let day2 = {
             date: weather.list[8].dt_txt,
             temp: weather.list[8].main.temp,
             humid: weather.list[8].main.humidity,
             wind: weather.list[8].wind.speed,
             icon: weather.list[8].weather[0].icon
           }
-
-          let day2 = {
+          let day3 = {
             date: weather.list[16].dt_txt,
             temp: weather.list[16].main.temp,
             humid: weather.list[16].main.humidity,
             wind: weather.list[16].wind.speed,
             icon: weather.list[16].weather[0].icon
           }
-          let day3 = {
+          let day4 = {
             date: weather.list[24].dt_txt,
             temp: weather.list[24].main.temp,
             humid: weather.list[24].main.humidity,
             wind: weather.list[24].wind.speed,
             icon: weather.list[24].weather[0].icon
           }
-          let day4 = {
+          let day5 = {
             date: weather.list[32].dt_txt,
             temp: weather.list[32].main.temp,
             humid: weather.list[32].main.humidity,
             wind: weather.list[32].wind.speed,
             icon: weather.list[32].weather[0].icon
           }
-          let day5 = {
-            date: weather.list[39].dt_txt,
-            temp: weather.list[39].main.temp,
-            humid: weather.list[39].main.humidity,
-            wind: weather.list[39].wind.speed,
-            icon: weather.list[39].weather[0].icon
-          }
           console.log(resp.data)
 
           const currentElem = document.createElement('div')
           currentElem.innerHTML = `
           <div class= "currentday">
-            <h3>Date:${current.date}</h3>
+            <h3>Current</h3>
             <h3>City: ${city} <img src="http://openweathermap.org/img/w/${current.icon}.png" alt="icon"></h3>
             <h3>Temperature: ${current.temp}</h3>
             <h3>Humidity: ${current.humid}</h3>
@@ -81,17 +80,25 @@ document.getElementById('search').addEventListener('click', event => {
           </div>
         `
           document.getElementById('today').append(currentElem)
-          
+
           let forecasts = [day1, day2, day3, day4, day5]
 
           document.getElementById('forecast').innerHTML = ''
-          forecasts.forEach(() => {
+          forecasts.forEach(day => {
             const forecastElem = document.createElement('div')
+            forecastElem.className = 'col-md-3 mb-3'
             forecastElem.innerHTML = `
-            <h3>date: ${weather.list[8].dt_txt} <img src="http://openweathermap.org/img/w/${current.icon}.png" alt="icon"></h3>
-            <h3>temp: ${weather.list[8].main.temp}</h3>
-            <h3>humid: ${weather.list[8].main.humidity}<h3>
-            <h3>wind: ${weather.list[8].wind.speed}</h3>
+              <div class="card" style="width: 10rem;">
+                  <img src="http://openweathermap.org/img/w/${day.icon}.png" class="card-img-top" alt="icon">
+                  <div class="card-body">
+                  <h5 class="card-title">${day.date}</h5>
+                  <p class="card-text">            
+                  <h3>temp: ${day.temp}</h3>
+                  <h3>humid: ${day.humid}<h3>
+                  <h3>wind: ${day.wind}</h3></p>
+                  </div>
+              </div>
+            </div>
         `
             document.getElementById('forecast').append(forecastElem)
           })
