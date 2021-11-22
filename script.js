ef2ac161e085dcf438a72e03f3f02505336815d7
LohasOT <oliverhoang93@gmail.com>
2021-11-22 00:09:47 -0800

    added axios for weathers

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-var weatherApiRootUrl = 'https://api.openweathermap.org';
-var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+// var weatherApiRootUrl = 'https://api.openweathermap.org';
+// var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
+
+axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=Irvine&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
+    .then(res => {
+
+      const weather = res.data
+      console.log(res.data)
+
+      axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat={33.6695}&lon={-117.8231}&appid=d91f911bcf2c0f925fb6535547a5ddc9`)
+
+    })
+
+
+  
+  // document.getElementById('search').addEventListener('click', event => {
+  //   event.preventDefault()
